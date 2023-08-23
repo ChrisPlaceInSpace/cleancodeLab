491e4eb8406e56944a7a3920b122c68c30843677
Chris Ostman <chr.lin.ost@gmail.com>
2023-08-23 11:25:54 +0200

    Added project

---
+++ LabbCC/Program.cs
@@ -0,0 +1,20 @@
+﻿using LabbCC;
+using System.Diagnostics;
+
+namespace MooGame
+{
+    class MainClass
+	{
+
+		public static void Main(string[] args)
+        {
+            Game game = new Game();
+            //Debug.Write(game.ToString());
+            //Debug.WriteLine("Test");
+            game.RunGame();
+            
+        }
+
+    }
+
+}
