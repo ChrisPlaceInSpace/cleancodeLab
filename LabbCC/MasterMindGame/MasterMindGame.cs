7b090deb0419559534597157f0eaa02451b37594
JockeMedKniven <JockeMedKniven@users.noreply.github.com>
2023-09-06 16:54:17 +0200

    Added small fix for clearing list with the playerDAO when running multiple times

--- LabbCC/MasterMindGame/MasterMindGame.cs
+++ LabbCC/MasterMindGame/MasterMindGame.cs
@@ -31,6 +31,7 @@ internal class MasterMindGame : IGame
                 catch (Exception ex) { _ui.Output(ex.ToString()); }
                 scoreBoard.UpdateScoreBoard(playerStats);
                 scoreBoard.PrintScoreBoard(playerStats);
+                playerStats.Clear();
                 gameOn = masterMindGameLogic.Continue();
             }
         }
